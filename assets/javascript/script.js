c379712fa4a0d3be7b28b9393be1ecf5bb6a2b02
Kevin <kphyde@gmail.com>
2018-09-05 19:44:04 -0700

    game mechanics work, simple css layout, all essential pieces, game win and loss states work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,6 +8,8 @@ var $targetNumber = $("#targetNumber");
 
 var $answerZone = $("#answerZone");
 
+var answer = 0;
+
 function guessNumber() {
     return Math.floor(Math.random() * (121 - 19)) + 19;
 }
@@ -16,11 +18,22 @@ function jewelNumber() {
     return Math.floor(Math.random() * (12 - 1)) + 1;
 }
 
+function numberMatch(target, answer) {
+    if (target === answer) {
+        console.log("GAME WON!")
+        gameWon();
+    } else if (answer > target) {
+        console.log("YOU LOSE!")
+        gameOver()
+    }
+}
+
 $(document).ready(function () {
     console.log(guessNumber());
     console.log(jewelNumber());
 
-    $targetNumber.text(guessNumber());
+    var target = guessNumber();
+    $targetNumber.text(target);
 
     $jewel1.attr("numberID", jewelNumber());
     $jewel2.attr("numberID", jewelNumber());
@@ -28,4 +41,10 @@ $(document).ready(function () {
     $jewel4.attr("numberID", jewelNumber());
 
     $answerZone.text("Your Guess Here");
+
+    $(".jewel").on("click", function() {
+        answer += parseInt($(this).attr("numberID"));
+        $answerZone.text(answer);
+        numberMatch(target,answer);
+    })
 });
