df6587d7ec5e8a0a6d071d52256e4e43f09d073f
Kevin <kphyde@gmail.com>
2018-09-06 16:41:23 -0700

    cleanup code and add comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,34 +4,36 @@ var $jewel2 = $("#jewel_2");
 var $jewel3 = $("#jewel_3");
 var $jewel4 = $("#jewel_4");
 
+
+// GAME VARIABLES
 var $targetNumber = $("#targetNumber");
-
 var $answerZone = $("#answerZone");
-
 var target;
-var answer = 0;
+var answer;
+var gamesWon = 0;
+var gamesLost = 0;
 
+// JQUERY IMAGE ELEMENTS
 var $jewelImgTarget = "<img class='jewel_img_target' src='assets/images/big_jewel.png' />";
 var $jewelImg1 = "<img class='jewel_img' src='assets/images/red_jewel.png' />";
 var $jewelImg2 = "<img class='jewel_img' src='assets/images/blue_jewel.png' />";
 var $jewelImg3 = "<img class='jewel_img' src='assets/images/purple_jewel.png' />";
 var $jewelImg4 = "<img class='jewel_img' src='assets/images/yellow_jewel.png' />";
-
 var gameResetButton = "<button class='reset' id='reset'>start a new game</button>";
-var gamesWon = 0;
-var gamesLost = 0;
 
-function guessNumber() {
+// GAME FUNCTIONS
+function guessNumber() { // GENERATE NUMBER TO MATCH
     return Math.floor(Math.random() * (121 - 19)) + 19;
 }
 
-function jewelNumber() {
+function jewelNumber() { // GENERATE RANDOM NUMBERS FOR JEWELS
     return Math.floor(Math.random() * (12 - 1)) + 1;
 }
 
-function numberMatch(target, answer) {
+function numberMatch(target, answer) { // CHECK IF TOTAL MATCHES TARGET NUMBER
     if (target === answer) {
         console.log("GAME WON!")
+        alert("You got it!");
         gamesWon++;
         $("#wins").text(gamesWon);
         gameWon();
@@ -42,6 +44,7 @@ function numberMatch(target, answer) {
         });
     } else if (answer > target) {
         console.log("YOU LOSE!");
+        alert("Oh shucks, you overshot the answer by " + parseInt(answer - target) + "!");
         gamesLost++;
         $("#losses").text(gamesLost);
         gameOver(target,answer);
@@ -53,7 +56,7 @@ function numberMatch(target, answer) {
     }
 }
 
-function gameReset() {
+function gameReset() { // REFRESH THE BOARD WITH NEW TARGET AND JEWELS
     target = guessNumber();
     answer = 0;
     $jewel1.empty();
@@ -66,40 +69,29 @@ function gameReset() {
     $("#targetNumber").hide().append($jewelImgTarget);
     $("#targetNumber").fadeTo(3000, 1);
     setTimeout(createJewels, 3000);
-    // createJewels();
 
     $answerZone.text("Your Guess Here");
-
-    // $(".jewel_img").on("click", function() {
-    //     console.log("ready");
-    //     answer += parseInt($(this).attr("numberID"));
-    //     $answerZone.text(answer);
-    //     numberMatch(target,answer);
-    // })
 }
 
-function gameOver(target,answer) {
-    alert("Oh shucks, you overshot the answer by " + parseInt(answer - target) + "!");
+function gameOver(target,answer) { // GAME LOSS STATE AND TALLY
     $(".jewel_img").jqFloat('stop');
     $(".jewel_img").animate({opacity: 0, top: "+=2000"}, 3000);
     $(".jewel_img").fadeOut(3000);
-    // $(".jewel").jqFloat({width:2000, height:2000, speed:200}).fadeOut(3000);
     $("#targetNumber").fadeTo(3000, .1);
     $answerZone.empty().append(gameResetButton);
 }
 
-function gameWon() {
-    alert("You got it!");
+function gameWon() { // GAME WIN AND TALLY
     $(".jewel_img").jqFloat({width:700, height:700, speed:200});
     $answerZone.empty().append(gameResetButton);
 }
 
-function createTargetJewel() {
+function createTargetJewel() { // FADE IN AND DISPLAY GREEN JEWEL
     $("#targetNumber").hide().append($jewelImgTarget);
     $("#targetNumber").fadeIn(3000);
 }
 
-function createJewels() {
+function createJewels() { // CREATE AND SET INPUT NUMBERS TO GUESSING JEWELS
     $jewel1.attr({"numberID": jewelNumber(), "style": "top: 0px, left: 0px"});
     $jewel1.hide().html($jewelImg1);
     $jewel1.fadeIn(500);
@@ -116,16 +108,14 @@ function createJewels() {
     $(".jewel_img").jqFloat({width:20, height:20, speed:2000});
 }
 
+// SET INITIAL STATE ON PAGE LOAD
 $(document).ready(function () {
-    console.log(guessNumber());
-    console.log(jewelNumber());
 
     target = guessNumber();
     $targetNumber.html("<h2 class='centered'>" + target + "</h2>");
 
     createTargetJewel();
     setTimeout(createJewels, 3000);
-    // createJewels();
 
     $answerZone.text("Your Guess Here");
 
